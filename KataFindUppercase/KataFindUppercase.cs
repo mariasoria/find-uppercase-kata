6d460f3381ae5ed9737aae6625fa5a17eba19249
Maria Soria <maria.it3@gmail.com>
2023-04-25 10:28:03 +0100

    First Commit

---
+++ KataFindUppercase/KataFindUppercase.cs
@@ -0,0 +1,10 @@
+namespace KataFindUppercase
+{
+    public class KataFindUppercase
+    {
+        public static string execute(string text)
+        {
+            return text;
+        }
+    }
+}
---
+++ KataFindUppercase/KataFindUppercaseTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace KataFindUppercase
+{
+    [TestFixture]
+    public class KataFindUppercaseTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(KataFindUppercase.execute("Hello"), "Hello");
+        }
+    }
+}
